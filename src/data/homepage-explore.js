c8497c0895d103f0975b2b7cefe31dcc15998ccd
Aviral-housing <aviral.jain@housing.com>
2024-08-24 17:04:07 +0530

    first commit1

---
+++ src/data/homepage-explore.js
@@ -0,0 +1,117 @@
+export const HomePageExplore = [
+    {
+        tag: 'Free',
+        courses : [
+            {
+                heading : "Learn HTML",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Learn CSS",
+                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Responsive Web design",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'New to coding',
+        courses : [
+            {
+                heading : "HTML",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "CSS",
+                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Responsive ",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Most popular',
+        courses : [
+            {
+                heading : "Java",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Python",
+                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "SCSS",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Skills paths',
+        courses : [
+            {
+                heading : "Flask",
+                description : "Flask is a lightweight and popular web framework written in Python. It is designed to be flexible, and easy to use applications.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Django",
+                description : "Django is a robust and popular web framework written in Python. It is designed to simplify the process of building web applications",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Fast API",
+                description : "FastAPI is a modern and high-performance web framework for building APIs with Python. ",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Career paths',
+        courses : [
+            {
+                heading : "Next.js",
+                description : "Next.js is a popular framework for building server-side rendered (SSR) and statically generated React applications. ",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Nuxt.js",
+                description : "Nuxt.js is a Vue.js framework for creating universal applications with Vue. server-side rendered Vue applications, SEO-friendly Vue applications",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Sanity",
+                description : "It is a headless content management system (CMS) that offers a flexible approach to managing content. ",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+]
